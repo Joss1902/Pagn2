e52e068a2fde833f574275a76efba59b050bcd3f
Joss1902 <jossmiclos@gmail.com>
2025-10-18 17:41:45 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,12 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const menuToggle = document.getElementById('menuToggle');
-    const navList = document.getElementById('navList');
 
-    // Toggle para el menú de navegación móvil
-    menuToggle.addEventListener('click', function() {
-        navList.classList.toggle('active');
-    });
 
-    // Cierra el menú si se hace clic fuera del mismo
-    document.addEventListener('click', function(event) {
-        const isClickInside = navList.contains(event.target) || menuToggle.contains(event.target);
-        
-        if (!isClickInside && navList.classList.contains('active')) {
-            navList.classList.remove('active');
-        }
-    });
-});
+
+
+
+
+
+
+
+
+
+
